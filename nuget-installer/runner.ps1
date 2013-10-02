aa3a9ab3e87ba99293549be030feb4136371daf9
Johan Leino <leino@outlook.com>
2013-10-02 14:21:11 +0200

    moved code into separate file and made some adjustments

---
+++ nuget-installer/runner.ps1
@@ -0,0 +1,34 @@
+function Build-Arguments {
+  $parameters = @()
+
+  $parameters += "install"
+  $parameters += "%NuGetInstaller.Package%"
+
+  if([String]::IsNullOrWhiteSpace("%NuGetInstaller.OutputDirectory%") -eq $false) {
+    $parameters += "-OutputDirectory"
+    $parameters += "%NuGetInstaller.OutputDirectory%"
+  }
+
+  if([String]::IsNullOrWhiteSpace("%NuGetInstaller.Options%") -eq $false) {
+	$("%NuGetInstaller.Options%" -split ' ') | Foreach-Object {
+		$parameters += $_
+	}
+  }
+
+  return $parameters
+}
+
+$nuget = Join-Path -Path '%teamcity.tool.NuGet.CommandLine.DEFAULT.nupkg%' -ChildPath 'tools\NuGet.exe'
+$arguments = Build-Arguments
+
+if(%NuGetInstaller.Log%) {
+	Write-Host "Executing nuget.exe"
+	Write-Host "source: $nuget"
+	Write-Host "using arguments: " -NoNewline
+
+	$arguments | Foreach-Object {
+		Write-Host "$_ " -NoNewline
+	} -End {Write-Host ""}
+}
+
+& $nuget $arguments | Out-String
